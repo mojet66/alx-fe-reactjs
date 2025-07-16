cd46991e77a9b72404865c843ef724f3176d84ed
mojet66 <oladosuemmanuel2017@gmail.com>
2025-07-16 11:27:55 +0100

    Apply inline CSS styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <main style={{ fontStyle: "italic", color: "red", fontSize: "40px" }}>
+      <marquee behavior="" direction="right">
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </marquee>
+    </main>
+  );
+}
+export default MainContent;
