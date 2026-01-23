ac7edee95512ce3d22c323dbe3813738be1d7ac9
mojet66 <oladosuemmanuel2017@gmail.com>
2026-01-23 14:10:05 +0100

    updated recipeStore in line 6

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,5 +3,6 @@ const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
 export default useRecipeStore;
