ab1d6c5f30313e69639230462253659667d94152
mojet66 <oladosuemmanuel2017@gmail.com>
2026-01-25 14:24:38 +0100

    i modified my recipestore and created a searchbar component

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,18 +2,47 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+  filteredRecipes: [],
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
-  deleteRecipe: (id) =>
+    set((state) => {
+      const updatedRecipes = [...state.recipes, newRecipe];
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filteredRecipes(updatedRecipes, state.searchTerm),
+      };
+    }),
+
+  setRecipes: (recipes) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      recipes: recipes,
+      filteredRecipes: filteredRecipes(recipes, state.searchTerm),
     })),
+
+  deleteRecipe: (id) =>
+    set((state) => {
+      const updatedRecipes = state.recipes.filter((recipe) => recipe.id !== id);
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filteredRecipes(updatedRecipes, state.searchTerm),
+      };
+    }),
+
   updateRecipe: (updatedRecipe) =>
+    set((state) => {
+      const updatedRecipes = state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      );
+      return {
+        recipes: updatedRecipes,
+        filteredRecipes: filteredRecipes(updatedRecipes, state.searchTerm),
+      };
+    }),
+  setSearchTerm: (term) =>
     set((state) => ({
-      recipes: state.recipes.map((recipe) => {
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe;
-      }),
+      searchTerm: term,
+      filteredRecipes: filteredRecipes(state.recipes, term),
     })),
 }));
 
